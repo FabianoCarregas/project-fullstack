b10f417d194fd1f84d51219241f19be78d21c8b6
unknown <fabianofac_@hotmail.com>
2021-09-06 13:58:06 -0300

    Basic Static Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">shop</h1>
+    <DataTable/>
+
     </div>
+    <Footer/>
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+
+
+const Footer = () =>  {
+    return (
+      
+          <footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por <a href="https://github.com/FabianoCarregas" target="_blank" rel="noreferrer">Fabiano Carregas</a></p>
+    <p className="text-light"><small><strong>Projeto FullStack</strong><br/>
+      Dev Java <a href="https://www.linkedin.com/in/fabiano-carregas/" target="_blank" rel="noreferrer">@Linkedin</a></small></p>
+  </div>
+</footer>
+     
+    );
+  }
+  
+  export default Footer;
+  
