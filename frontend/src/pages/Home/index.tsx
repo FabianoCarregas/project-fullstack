b1d70028db3b8a95f5f663d107fa7ba6fdc2c391
unknown <fabianofac_@hotmail.com>
2021-09-10 10:18:56 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,25 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () =>  {
     return (
-        <div>
-        <h1>Page home</h1>
-      </div>
+        <>
+        <NavBar />
+        <div className="container">
+        <div className="jumbotron">
+            <h1 className="display-4">Fab-lous-Sales</h1>
+            <p className="lead">Check your sales out, in real time with our platform! </p>
+            <hr/>
+            <p>This Application you provide you with an immersive experience from an app developed from scratch, through  Spring Boot backend, Angular frontend Frameworks.</p>
+            <Link className="btn btn-primary btn-lg" to="/dashboard">
+                Dashboard
+            </Link>    
+
+        </div>
+    </div>
+    <Footer />
+    </>
     );
   }
   
